3d692025596abab9a108013bae0b6dd68a65c8f9
manihundal <31777114+manihundal@users.noreply.github.com>
2018-10-20 13:01:38 -0700

    Update Country.java

--- src/com/r/model/Country.java
+++ src/com/r/model/Country.java
@@ -8,7 +8,7 @@ import com.r.model.Land;
  * it has methods which can change the state and return the
  * state of the object.
  * 
- * @author Raghav verma
+ * @author Manjinder Kaur 
  * @version 1.0.0.0
  */
 public class Country extends Land {
