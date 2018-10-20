440c8274cc2332db1a86607c27e5516109d0ae4f
Nayan3103 <43422625+Nayan3103@users.noreply.github.com>
2018-10-20 16:12:57 -0400

    Update ExchangeCards.java

--- src/com/r/utility/ExchangeCards.java
+++ src/com/r/utility/ExchangeCards.java
@@ -4,7 +4,7 @@ package com.r.utility;
  * This is enumeration for the cards used in game.
  * It has three types: infantry, cavalry and artillery
  * 
- * @author Raghav verma
+ * @author Nayan Girase
  * @version 1.0.0.0
  */
 public enum ExchangeCards {
